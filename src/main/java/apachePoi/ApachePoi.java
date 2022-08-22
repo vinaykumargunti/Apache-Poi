e3b51d5ba73265ab2c4bf633222e0bc310f242da
Vinaythebeginner <vinaykumar.gunti3@gmail.com>
2022-08-23 00:52:01 +0530

    Full Name

--- src/main/java/apachePoi/ApachePoi.java
+++ src/main/java/apachePoi/ApachePoi.java
@@ -9,7 +9,7 @@ public class ApachePoi {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		double y = (double) n;
-		System.out.println("VinayKu");
+		System.out.println("VinayKumar");
 		double x = 1 / y;
 		System.out.println("Vinay");
 		String s = Double.toString(x);
